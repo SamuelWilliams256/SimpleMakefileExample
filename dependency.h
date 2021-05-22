9349360b242c8432adca8e7cabe4dada44278c05
SamuelWilliams256 <25990267+SamuelWilliams256@users.noreply.github.com>
2021-05-22 12:11:37 -0400

    Testing

---
+++ dependency.h
@@ -0,0 +1,8 @@
+#ifndef DEPENDENCY_H
+#define DEPENDENCY_H
+
+#include <string>
+
+std::string hello_world = "Hello world!\n";
+
+#endif
---
+++ hello_world.cpp
@@ -0,0 +1,9 @@
+#include "dependency.h"
+
+#include <iostream>
+
+int main ()
+{
+     std::cout << hello_world;
+       return 0;
+}
